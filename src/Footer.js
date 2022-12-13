b5057c4ded124aa0e3438e9d1a9aa20b582c37f3
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-12-13 18:47:45 -0300

    useState, props, lists and other stuff

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    const today=new Date();
+
+
+    return (
+      <>
+       <footer>
+        <p>Copyright &copy; {today.getFullYear()}</p>
+       </footer>
+      </>
+    );
+  }
+  
+  export default Footer;
