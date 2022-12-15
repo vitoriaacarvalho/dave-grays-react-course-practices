91bcdc956de54ef4ed00a27ca987e2ebdb3248a0
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-12-15 11:01:54 -0300

    learning props and prop drilling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ import Header from './Header';
 import './App.css';
 import Content from './Content';
 import Footer from './Footer';
+import { useState } from 'react';
 
 function App() {
+  const [items,setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item:"one half pound bag of cocoa covered almonds unsalted"
+    },
+    {
+      id: 2,
+      checked: false,
+      item:"Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item:"Item 3"
+    }
+  ]);
+
+  const handleCheck=(id)=>{
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  };
+
+  const handleDelete=(id)=>{
+    const listItems=items.filter((item)=> item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
+
   return (
     <div className="App">
       <Header/>
-      <Content/>
+      <Content 
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+        />
       <Footer/>
     </div>
   );
